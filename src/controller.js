b43ec856923eb321a3dae74522e1f4d5eeb69a88
moonchanghee <ckdgml228@gmail.com>
2022-04-24 12:20:52 +0900

    changhee commit - button, dropzone 이벤트리스너 추가

--- src/controller.js
+++ src/controller.js
@@ -1,8 +1,11 @@
 const modal = document.querySelector('.modal');
 const contents_todo = document.querySelector(".todo");
-const contents_inprogress = document.querySelector(".inprogress")
-const contents_done = document.querySelector(".done")
+const contents_inprogress = document.querySelector(".inprogress");
+const contents_done = document.querySelector(".done");
 const headerBtn = document.querySelector(".headBtn")
+const closeBtn = document.querySelector(".closeBtn")
+const modal_state = document.querySelector("#modal-state")
+const modal_priority = document.querySelector("#modal-priority")
 
 
 import Item from './view/Item'
@@ -11,43 +14,74 @@ export default class controller {
 
     constructor() {
         this.addEvent()
-        this.render()
     }
 
-    addEvent(){
-        headerBtn.addEventListener('click' , () => {
-        console.log("asdf")
+    addEvent() {
+        headerBtn.addEventListener('click', () => {
+            modal.classList.remove('hidden');
         })
-}
+        closeBtn.addEventListener('click', (e) => {
+            console.log(modal_priority.options)
+            let state = modal_state.options[modal_state.selectedIndex].value
+            let priority = modal_priority.options.selectedIndex
+            console.log("priority", priority)
+            addItems(state)
+        })
+        let count = 1
+        const addItems = (s) => {
+            if ("ToDo" == s) {
+                newTodo(contents_todo, count)
+            } else if ("In progress" == s) {
+                newTodo(contents_inprogress, count)
+            } else {
+                newTodo(contents_done, count)
+            }
+            modal.classList.add('hidden');
+        }
+        const newTodo = (state, id) => {
+            console.log("count", count)
+            let addItem = document.createElement("div")
+            addItem.innerHTML = Item(id)
+            state.appendChild(addItem)
+            count++
+        }
+
+        document.body.onclick = (e) => {
+            if (e.target.className == "addbtn") {
+                console.log("asdf")
+            }
+        }
+
+        document.body.addEventListener('mouseover',function(e) {
+            if (e.target.className == "add") {
+                e.target.addEventListener("dragstart", (e) => {
+                    console.log(e)
+                    e.dataTransfer.setData("text/plain", e.target.id)
+                });
+            }
+            if (e.target.className == "dropzone") {
+                e.target.addEventListener("dragover", e => {
+                    console.log("dragover")
+                    e.preventDefault();
+                    e.target.classList.add("kanban__dropzone--active");
+                });
+                e.target.addEventListener("dragleave", () => {
+                    console.log("dragleave")
+                    e.target.classList.remove("kanban__dropzone--active");
+                });
 
 
-    render(){
-        contents_todo.innerHTML =  Item
+                e.target.addEventListener("drop", e => {
+                    e.preventDefault();
+                    console.log("dragleave")
+                    dropZone.classList.remove("kanban__dropzone--active");
+
+                });
+            }
+
+
+        },false);
 
     }
-    // addBtn(){
-    //     console.log("모달")
-    //     modal.classList.remove('hidden');
-    // }
-    //
-    //
-    //
-    // closeBtn(){
-    //     modal.classList.add('hidden');
-    //     let modal_state = target.options[target.selectedIndex].value
-    //     addList(modal_state)
-    // }
-    //
-    // addList(s){
-    //     if("todo" == s ){
-    //         console.log("todo")
-    //         contents_todo.appendChild(btnex)
-    //     }else if("inprogress" == s){
-    //         console.log("inprogress")
-    //     }else{
-    //         console.log("done")
-    //     }
-    // }
-
 }
 
