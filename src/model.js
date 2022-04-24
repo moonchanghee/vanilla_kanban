8969e30b21559eacad4703ee9bd4e353956d0014
moonchanghee <ckdgml228@gmail.com>
2022-04-24 19:21:04 +0900

    changhee commit - insertItems, getItems 메서드 추가

--- src/model.js
+++ src/model.js
@@ -1,26 +1,33 @@
 
 export default class Model{
-    constructor() {
-        this.controller = controller
-        this.title = title
-        this.content = content
-        this.createAt = createAt
-        this.endAt = endAt
-        this.priority = priority
-        this.state = state
 
+    insertItems(d){
+        const data = read();
+        data.push(d)
+        save(data)
+        return d
     }
 
-
-
-    updateStorage(){
-
-    }
-    deleteStorage(){
-
-    }
-    insertStorage(key,item){
-    localStorage.setItem(key , item)
+    getItems(){
+        const data = read()
+        return data
     }
 
+}
+
+const read = () => {
+    const json = localStorage.getItem("kanban-data");
+
+
+    if (!json) {
+        return [];
+    }
+
+    return JSON.parse(json);
+}
+
+const save = (data) => {
+    localStorage.setItem("kanban-data", JSON.stringify(data));
+    let datas = read()
+    console.log(datas)
 }
