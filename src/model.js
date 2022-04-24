61d65b8a25eac92145230bd24ecabe8fd4648af3
moonchanghee <ckdgml228@gmail.com>
2022-04-24 12:19:09 +0900

    changhee commit - 초기환경 세팅

--- src/model.js
+++ src/model.js
@@ -1,9 +1,26 @@
 
 export default class Model{
     constructor() {
+        this.controller = controller
+        this.title = title
+        this.content = content
+        this.createAt = createAt
+        this.endAt = endAt
+        this.priority = priority
+        this.state = state
 
     }
 
 
+
+    updateStorage(){
+
+    }
+    deleteStorage(){
+
+    }
+    insertStorage(key,item){
+    localStorage.setItem(key , item)
+    }
 
 }
