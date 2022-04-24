6a2eb029f55a532cb4d5216fa5f79c2b5f97f540
moonchanghee <ckdgml228@gmail.com>
2022-04-24 12:17:57 +0900

    changhee commit - dropzone 추가

--- src/view/Item.js
+++ src/view/Item.js
@@ -1,7 +1,14 @@
-export default function Item(){
+export default function Item(e){
     return `
-    adf
-    `
+<div class = "add" id = ${e} draggable="true" >
+    제목 : 제목
+    생성일 : 생성일
+    완료일 :완료일ㅇㄹㅇ 
+    <button class = "addbtn">버튼</button>
+    <button class = "upbtn">수정</button>
+</div>
+<div class = "dropzone"> </div>
+`;
 
 
 }
