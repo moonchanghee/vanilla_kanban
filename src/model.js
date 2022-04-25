4d699a3e3c2b53b9b8767aadc88b21de0611f839
moonchanghee <ckdgml228@gmail.com>
2022-04-26 01:19:23 +0900

    changhee commit - sortItemList 메서드 추가, 스토리지 구조 변경

--- src/model.js
+++ src/model.js
@@ -1,63 +1,86 @@
 
 export default class Model{
 
-    insertItems(d){
-        const data = read();
-        data.push(d)
-        save(data)
+    insertItems(d,state){
+
+        const data = this.read();
+        const column = data.find(e => e.id == state)
+        column.items.push(d)
+        this.save(data)
         return d
     }
-
     getItems(){
-        const data = read()
+        const data = this.read()
         return data
     }
-
     selectItem(itemId){
-        const data = read().find(e => e[0].id == itemId)
-
+        const data = this.read().find(e => e[0].id == itemId)
         return data[0]
     }
 
-
     updateItem(itemId , newData){
-        const upDateData = read().find(e => e[0].id == itemId)
+        const upDateData = this.read().find(e => e[0].id == itemId)
         // upDateData.item_content = newData.item_content
         // upDateData.item_title = newData.item_title
         // upDateData.item_date = newData.item_date
         // upDateData.item_state = newData.item_state
         // upDateData.item_priority = newData.item_priority
+    }
 
+    sortItemList(e){
+        const data = this.read();
+
+        if(e == "low"){
+            data.forEach((e) => {
+                e.items.sort((a,b) => {
+                    return (Number(a[0].item_priority_val) - Number(b[0].item_priority_val))
+                })
+            })
+        }else if(e == "high"){
+            data.forEach((e) => {
+                e.items.sort((a,b) => {
+                    return (Number(b[0].item_priority_val) - Number(a[0].item_priority_val))
+                })
+            })
+        }
+
+        this.save(data);
 
     }
+
+
+
 
     deleteItem(itemId) {
-        const data = read();
-        let newData = []
+        const data = this.read();
+
         data.forEach((e) => {
-            if(e[0].id !== itemId){
-                newData.push(e)
+            for(let i =0 ; i<e.items.length; i++){
+                if(e.items[i][0].id == itemId ){
+                    console.log("d",e.items[i][0].id)
+                    e.items.splice(e.items.indexOf(e.items[i][0].id),1)
+                    //삭제 수정
+                }
             }
         })
-
-        console.log("newData",newData)
-        save(newData);
+        this.save(data);
     }
 
-}
+    read = () => {
+        const json = localStorage.getItem("kanban-data");
+        if (!json) {
+            return [{id : "ToDo" , items: [] } , {id : "In_progress" , items: [] } , {id : "Done" , items: [] }]
+        }
 
-const read = () => {
-    const json = localStorage.getItem("kanban-data");
-    if (!json) {
-        return [];
+        return JSON.parse(json);
     }
-    return JSON.parse(json);
+
+    save = (data) => {
+        localStorage.setItem("kanban-data", JSON.stringify(data));
+        let datas = this.read()
+        console.log(datas)
+    }
 }
 
-const save = (data) => {
-    localStorage.setItem("kanban-data", JSON.stringify(data));
-    let datas = read()
-    console.log(datas)
-}
 
 
