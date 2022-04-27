72ec83bfd999bcf18aeaa68fc7f9acef912ebc6b
moonchanghee <ckdgml228@gmail.com>
2022-04-27 14:16:52 +0900

    changhee commit - 드랍 아이템 추가

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,6 @@
 const modal = document.querySelector('.modal');
-const contents_todo = document.querySelector(".Todo");
+const contents_todo = document.querySelector(".ToDo");
+const head_container = document.querySelector(".head-container")
 const contents_inprogress = document.querySelector(".In_progress");
 const contents_done = document.querySelector(".Done");
 const modal_state = document.querySelector("#modal-state")
@@ -11,125 +12,164 @@ const kanban_wrap = document.querySelector(".kanban_wrap")
 const select = document.querySelector(".select")
 
 import Item from './view/Item'
-import Modal from './view/Modal'
 import Model from './model'
-import Vc from './view/Vc'
+import Modal from './view/Modal'
 
 export default class controller {
     constructor() {
         this.model = new Model()
-        this.vc = new Vc()
-        this.getItemList()
         this.onClickAddButton()
         this.onClickUpdateButton()
         this.onChangeSortSelect()
         this.onClickCloseModal()
+        this.onClickUpdateModal()
+        this.onClickDeleteButton()
+        this.getItemList()
     }
 
-    getItemList(){
-        this.model.getItems().forEach((e) => {
-            for(let i =0 ; i<e.items.length; i++){
-                this.addItems(e.items[i][0])
-            }
-        })
-    }
+
 
     selectChange(){
         select.addEventListener('change',(e) => {
             let sort_name = select.options[select.selectedIndex].getAttribute("class")
             if(sort_name == "sel_high"){
-                console.log("sel_high")
                 this.model.sortItemList("high")
 
             }else if(sort_name == "sel_low"){
-                console.log("sel_low")
                 this.model.sortItemList("low")
             }
-            // let removeItems = document.getElementsByClassName("todoItem")
-            // for (var i = 0; i < removeItems.length; i++) {
-            //     removeItems[i].remove();
-            // }
-
-            // this.getItemList()
+            location.reload()
         })
     }
 
     closeModal() {
         modal.addEventListener('click',(e) => {
+            let state = modal_state.options[modal_state.selectedIndex].value
+            let data = [{
+                id : this.uuid(),
+                item_content : modal_contents.value,
+                item_title : title.value,
+                item_date :  date.value,
+                item_state : modal_state.options[modal_state.selectedIndex].value,
+                item_priority : modal_priority.options[modal_priority.selectedIndex].text,
+                item_priority_val : modal_priority.options[modal_priority.selectedIndex].getAttribute("value")
+            }]
+
             if(e.target.className ==  "closeBtn"){
-                console.log("closeBtn")
-                let state = modal_state.options[modal_state.selectedIndex].value
-                let priority_val = modal_priority.options[modal_priority.selectedIndex].getAttribute("value")
-                let priority = modal_priority.options[modal_priority.selectedIndex].text
-                let data = [{
-                    id : this.uuid(),
-                    item_content : modal_contents.value,
-                    item_title : title.value,
-                    item_date :  date.value,
-                    item_state : state,
-                    item_priority : priority,
-                    item_priority_val : priority_val
-                }]
-                const newItem = this.model.insertItems(data,state  )
-
-                this.addItems(newItem[0])
-            }else if(e.target.className ==  "updateSuccess"){
-                    console.log("updatesuccess")
+                this.model.insertItems(data[0],state)
+                this.addItems(data[0])
+                modal.classList.add('hidden');
             }
         })
     }
 
-    successBtn(){
+
+    upDateModal(id){
         modal.addEventListener('click',(e) => {
-            if(e.target.className  == "successBtn"){
-                console.log("successBtn")
+            let data = [{
+                id : id,
+                item_content : modal_contents.value,
+                item_title : title.value,
+                item_date :  date.value,
+                item_state : modal_state.options[modal_state.selectedIndex].value,
+                item_priority : modal_priority.options[modal_priority.selectedIndex].text,
+                item_priority_val : modal_priority.options[modal_priority.selectedIndex].getAttribute("value")
+            }]
+
+            if(e.target.className  == "succBtn"){
+                this.model.updateItem(id, data[0])
+                modal.classList.add('hidden');
+                location.reload()
             }
         })
     }
 
-    updateItem(){
+    addItemBtn(){
+        head_container.addEventListener('click',(e) => {
+            if(e.target.className ==  "headBtn"){
+                modal.classList.remove('hidden');
+            }
+        })
+    }
+
+    updateBtn(){
         kanban_wrap.addEventListener('click',(e) => {
             if(e.target.className ==  "upbtn"){
-                // modal.innerHTML = Modal(0)
-                // modal.appendChild()
-                // modal.classList.remove('hidden');
+                modal.classList.remove('hidden');
                 let updateData = this.model.selectItem(e.path[1].id)
                 title.value = updateData.item_title
                 modal_contents.value = updateData.item_content
                 date.value = updateData.item_date
-
-                // let newData = [{
-                //     id : updateData.id,
-                //     item_content : modal_contents.value,
-                //     item_title : title.value,
-                //     item_date :  date.value,
-                //     item_state : modal_state.options[modal_state.selectedIndex].value,
-                //     item_priority : modal_priority.options.selectedIndex
-                // }]
+                this.upDateModal(e.path[1].id)
             }
-            else if(e.target.className ==  "delbtn"){
-                console.log(e.path[3].childNodes[1])
-                // console.log("e.path[1].id",e.path[1].id)
-                this.model.deleteItem(e.path[1].id)
-                let del_node = document.getElementById(e.path[1].id);
-                del_node.remove()
-            }})
-            }
+    })
+    }
 
+    deletBtn(){
+            kanban_wrap.addEventListener('click',(e) => {
+                    if(e.target.className ==  "delbtn"){
+                        this.removeItem(e.path[1].id)
+                    }})
+        }
+
+    drag_drop(){
+        var pre_id
+        document.body.childNodes[3].querySelectorAll(".dropzone").forEach((e) => {
+            e.addEventListener("dragover" , (e) => {
+                e.preventDefault();
+                e.target.classList.add("dropzone_active")
+            })
+            e.addEventListener("dragleave" , (e) => {
+                e.target.classList.remove("dropzone_active")
+            })
+            e.addEventListener("drop" , (e) => {
+                e.preventDefault();
+                e.target.classList.remove("dropzone_active")
+                var move_state = e.path[0].id
+                let data = this.model.selectItem(pre_id)
+                if(data){
+                    this.removeItem(pre_id)
+                    data.item_state = move_state
+                    this.model.insertItems(data,move_state)
+                    location.reload()
+                }
+            })
+        })
+        document.body.childNodes[3].querySelectorAll(".todoItem").forEach((e) => {
+            e.addEventListener("dragstart" , (e) => {
+                pre_id = e.target.id
+            })
+        })
+    }
+
+    getItemList(){
+        this.model.getItems().forEach((e) => {
+            for(let i =0 ; i<e.items.length; i++){
+                this.addItems(e.items[i])
+            }
+        })
+    }
+
+    removeItem(e){
+         let del_parentNode = document.getElementById(e).parentNode
+         if(del_parentNode){
+             this.model.deleteItem(e)
+             del_parentNode.remove()
+         }
+    }
     addItems(s) {
         if ("ToDo" == s.item_state) {
             this.newTodo(contents_todo, s)
-        } else if ("In progress" == s.item_state) {
+        } else if ("In_progress" == s.item_state) {
             this.newTodo(contents_inprogress, s)
         } else if("Done" == s.item_state) {
             this.newTodo(contents_done, s)
         }
-        modal.classList.add('hidden');
     }
 
     newTodo (state, s) {
         let addItem = document.createElement("div")
-        addItem.innerHTML = Item(s)
+        addItem.innerHTML = Item(s, s.item_state)
         state.appendChild(addItem)
         this.drag_drop()
     }
@@ -142,50 +182,32 @@ export default class controller {
     }
 
 
-    drag_drop(){
-        document.body.childNodes[3].querySelectorAll(".dropzone").forEach((e) => {
-            e.addEventListener("dragover" , (e) => {
-                e.preventDefault();
-                e.target.classList.add("dropzone_active")
-            })
-            e.addEventListener("dragleave" , (e) => {
-                e.target.classList.remove("dropzone_active")
-            })
-            e.addEventListener("drop" , (e) => {
-                e.preventDefault();
-                e.target.classList.remove("dropzone_active")
-                console.log(e)
-                console.log(e.path[2].className)
-                console.log(e.path[1].childNodes[1].id)
-                let move_chId = e.path[1].childNodes[1].id
-                let move_state = e.path[2].className
-
-                // let pre_id =
-            })
-            // e.addEventListener("dragstart" , (e) => {
-            //     console.log("dragstart")
-            //     console.log(e.target.id)
-            // })
-        })
-
-    }
 
 
 
     onClickAddButton(){
-        this.vc.addItemBtn()
+        this.addItemBtn()
     }
+
     onClickUpdateButton(){
-        this.updateItem()
+        this.updateBtn()
     }
+
     onChangeSortSelect(){
         this.selectChange()
     }
+
     onClickCloseModal(){
         this.closeModal()
     }
 
+    onClickUpdateModal(){
+        this.upDateModal()
+    }
 
+    onClickDeleteButton(){
+        this.deletBtn()
+    }
 }
 
 
--- src/view/Vc.js
+++ src/view/Vc.js
@@ -1,83 +1,72 @@
-const head_container = document.querySelector(".head-container")
-const modal = document.querySelector('.modal');
-const select = document.querySelector(".select")
-const contents_todo = document.querySelector(".todo");
-const contents_inprogress = document.querySelector(".inprogress");
-const contents_done = document.querySelector(".done");
-const dropzone = document.querySelectorAll(".dropzone")
-const dynamicNList = document.body.childNodes;
-
-import Item from "./Item";
-import Modal from "./Modal"
-
-
-
-export default class Vc{
-    constructor() {
-        this.addItemBtn()
-    }
-
-
-    addItemBtn(){
-        head_container.addEventListener('click',(e) => {
-            if(e.target.className ==  "headBtn"){
-                modal.classList.remove('hidden');
-            }
-        })
-    }
-
-    // selectChange(){
-    //     select.addEventListener('change',(e) => {
-    //         let sort_name = select.options[select.selectedIndex].getAttribute("class")
-    //         if(sort_name == "sel_high"){
-    //             console.log("sel_high")
-    //         }else if(sort_name == "sel_low"){
-    //             console.log("sel_low")
-    //         }
-    //     })
-    // }
-
-    addItems(s){
-        console.log(s)
-        // if ("ToDo" == s.item_state) {
-        //     this.newTodo(contents_todo, s)
-        // } else if ("In progress" == s.item_state) {
-        //     this.newTodo(contents_inprogress, s)
-        // } else if("Done" == s.item_state) {
-        //     this.newTodo(contents_done, s)
-        // }
-        // modal.classList.add('hidden');
-    }
-
-    // newTodo(state, s){
-    //     let addItem = document.createElement("div")
-    //     addItem.innerHTML = Item(s)
-    //     state.appendChild(addItem)
-    // }
-
-    //         e.target.addEventListener("dragstart", (e) => {
-    //             e.dataTransfer.setData("text/plain", e.target.id)
-    //         });
-
-
-    // drag_drop(){
-    //     document.body.childNodes[3].querySelectorAll(".dropzone").forEach((e) => {
-    //         e.addEventListener("dragover" , (e) => {
-    //             e.preventDefault();
-    //             e.target.classList.add("dropzone_active")
-    //         })
-    //         e.addEventListener("dragleave" , (e) => {
-    //             e.target.classList.remove("dropzone_active")
-    //         })
-    //         e.addEventListener("drop" , (e) => {
-    //             e.preventDefault();
-    //             console.log("drop")
-    //             e.target.classList.remove("dropzone_active")
-    //             console.log(e.path[1].querySelector(".itemTodo"))
-    //             console.log(e.path[1].childNodes[1].id)
-    //
-    //         })
-    //     })
-    //
-    // }
-}
+// // const head_container = document.querySelector(".head-container")
+// // const modal = document.querySelector('.modal');
+// // const select = document.querySelector(".select")
+// // const contents_todo = document.querySelector(".todo");
+// // const contents_inprogress = document.querySelector(".inprogress");
+// // const contents_done = document.querySelector(".done");
+// // const dropzone = document.querySelectorAll(".dropzone")
+// // const dynamicNList = document.body.childNodes;
+//
+//
+// import Item from "./Item";
+//
+// export default class Vc{
+//
+//     constructor() {
+//         this.onClickDeleteButton()
+//         this.onClickAddButton()
+//     }
+//
+//     newTodo (state, s) {
+//         let addItem = document.createElement("div")
+//         addItem.classList.add('ItemDiv');
+//         addItem.innerHTML = Item(s)
+//         state.appendChild(addItem)
+//         this.drag_drop()
+//     }
+//
+//
+//     addItemBtn(){
+//         head_container.addEventListener('click',(e) => {
+//             if(e.target.className ==  "headBtn"){
+//                 modal.classList.remove('hidden');
+//             }
+//         })
+//     }
+//
+//     deletBtn(){
+//         kanban_wrap.addEventListener('click',(e) => {
+//             if(e.target.className ==  "delbtn"){
+//                 this.removeItem(e.path[1].id)
+//             }
+//         })
+//     }
+//
+//
+//     addItems(s) {
+//         if ("ToDo" == s.item_state) {
+//             this.newTodo(contents_todo, s)
+//         } else if ("In_progress" == s.item_state) {
+//             this.newTodo(contents_inprogress, s)
+//         } else if("Done" == s.item_state) {
+//             this.newTodo(contents_done, s)
+//         }
+//         modal.classList.add('hidden');
+//     }
+//
+//
+//     uuid () {
+//         return 'xx'.replace(/[xy]/g, function(c) {
+//             let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+//             return v.toString(16);
+//         });
+//     }
+//
+//     onClickDeleteButton(){
+//         this.deletBtn()
+//     }
+//     onClickAddButton(){
+//         this.addItemBtn()
+//     }
+//
+// }
